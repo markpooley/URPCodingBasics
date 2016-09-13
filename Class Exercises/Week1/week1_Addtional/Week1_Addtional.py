ac97ab372bd71254fa56f341c08336dfc48b6eda
Mark Pooley <mawwwk@gmail.com>
2016-09-13 14:45:30 -0500

    updating main exercises and addtional exercises

---
+++ Class Exercises/Week1/week1_Addtional/Week1_Addtional.py
@@ -0,0 +1,32 @@
+#week1 additional problems
+
+
+#convert mph to feet per second
+def converter(mph)
+
+#Count the number of times a target number occurs in a list
+#Output a the following string " 'target' occurs 'x' times in the list: 'ls'"
+#with the words in single quotes filled in
+def targetCounter(ls,target):
+    return None
+
+#create a function that randomly outputs choices from a menu of options
+# this should impement the "random" library to randomly select a choice
+# from three lists of menu options. I did you a favor giving you the list options
+def menu():
+    
+    appetizer = ["hummus plate","kimchi","spinach artichoke dip","bacon","chili cheese fries","chips & salsa"]
+    mainCourse = ["all the bacon and eggs you have","Mulligan's Steak","waffles with whip cream","Calzone","the meat tornado burrito (it literally killed a guy)","Paunch Burger", "Bacon wrapped Shrimp (My Number 1 favorite food wrapped around my number 3 favorite food"]
+    desert = ["Chocolate Chip Cookie","Ice Cream Sundae","Creme Brulee","Chocolate Cake","just whipped cream","brownie"]
+    
+    return None
+
+
+
+#Just creating a list here for testing the first function.
+ls = [x for x in range(15)] + [x for x in range(12)]
+
+
+print targetCounter(ls,12)
+print menu()
+print converter(88)
